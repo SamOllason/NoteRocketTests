9774ef48514650104976bcf00bd376cd72c2a829
Sam Ollason <sam.ollason@aplan.co.uk>
2024-10-09 08:43:30 +0100

    Add project files.

---
+++ NotePageTests.cs
@@ -0,0 +1,15 @@
+﻿using NoteRocket.Client.Components;
+
+namespace NoteRocketTests;
+public class NotePageTests : BunitTestContext
+{
+    [Test]
+    public void PageHasTitle()
+    {
+        // Arrange
+        var cut = RenderComponent<NotePage>();
+
+        // Assert that content of the paragraph shows counter at zero
+        cut.Find("h1").MarkupMatches("<h1>Title 1</h1>");
+    }
+}
