1ece91f4a5655b53d9ce61b4d1c4ba17d9b93542
Sam Ollason <sam.ollason@aplan.co.uk>
2024-10-14 09:12:22 +0100

    Adding Category field to Notes, adding Category buttons to Notes for filtering page, adding tests.

--- NotePageTests.cs
+++ NotePageTests.cs
@@ -4,12 +4,22 @@ namespace NoteRocketTests;
 public class NotePageTests : BunitTestContext
 {
     [Test]
-    public void PageHasTitle()
+    public void NotePage_RendersTitle()
     {
         // Arrange
         var cut = RenderComponent<NotePage>();
 
         // Assert that content of the paragraph shows counter at zero
         cut.Find("h1").MarkupMatches("<h1>Ti</h1>");
+    }
+
+    [Test]
+    public void NotePage_RendersCategoryButtons()
+    {
+        // Arrange
+        var cut = RenderComponent<NotePage>();
+
+        // Assert that content of the paragraph shows counter at zero
+        cut.Find("button").MarkupMatches("<button>Holidays</button>");
     }
 }
--- NotesTests.cs
+++ NotesTests.cs
@@ -1,15 +1,28 @@
-﻿using NoteRocket.Client.Pages;
+﻿using NoteRocket.Client.Components;
+using NoteRocket.Client.Pages;
 
 namespace NoteRocketTests;
 public class NotesTests : BunitTestContext
 {
     [Test]
-    public void PageHasTitle()
+    public void NotePage_RendersTitle()
     {
         // Arrange
         var cut = RenderComponent<Notes>();
 
         // Assert that content of the paragraph shows counter at zero
         cut.Find("h1").MarkupMatches("<h1>Notes</h1>");
+    }
+
+    [Test]
+    public void NotePage_RendersCategoryButtons()
+    {
+        // Arrange
+        var cut = RenderComponent<Notes>();
+
+        // Assert
+        cut.Find("#button-Personal").MarkupMatches("<button id='button-Personal'>Personal</button>");
+        //cut.Find("button").MarkupMatches("<button>Personal</button>");
+        //cut.Find("button").MarkupMatches("<button>Random</button>");
     }
 }
