4b099ed179f3682ab30a60af2bb46330d333e496
Sam Ollason <sam.ollason@aplan.co.uk>
2024-10-18 13:39:27 +0100

    WIP - adding tests to Notes repository

---
+++ NotesRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using NoteRocket.Client.Models;
+namespace NoteRocketTests;
+
+[TestFixture]
+public class NoteRepositoryTests
+{
+
+    [Test]
+    public void UpdateNote_NoteIdDoesntMatchNote_Aborts()
+    {
+        //Setup
+
+        var id = 1;
+        var note = new Note { Id = 1, Title = "Shopping list", Body = "carrot, broccoli...", Category = Category.Personal };
+        //Arrange
+        var bob = new NotesRepository();
+        var outcome = bob.UpdateNote(id, note);
+
+        //Act
+        Assert.AreEqual(outcome, "updated");
+    }
+}
