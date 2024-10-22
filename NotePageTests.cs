f6aa134a7ec38c340190a9c509924851bbc03971
Sam Ollason <sam.ollason@aplan.co.uk>
2024-10-22 11:17:24 +0100

    Adding test for navigating user back home after save

--- NotePageTests.cs
+++ NotePageTests.cs
@@ -1,18 +1,37 @@
 ﻿using NoteRocket.Client.Components;
+using NoteRocket.Client.Models;
 
 namespace NoteRocketTests;
 public class NotePageTests : BunitTestContext
 {
+    //    [Test]
+    //    public void NotePage_RendersTitle()
+    //    {
+    //        // Arrange
+    //        //var cut = RenderComponent<NotePage>(parameters => parameters);
+
+    //        var cut = RenderComponent<NotePage>(parameters => parameters
+    //            .Add(p => p.Id, 1));
+
+    //        // Assert
+    //        cut.Find("h1").MarkupMatches("<h1>Shopping list</h1>");
+    //    }
+
     [Test]
-    public void NotePage_RendersTitle()
+    public void NotePage_ClickingClose_NavigatesUserBackToNotesHome()
     {
         // Arrange
-        //var cut = RenderComponent<NotePage>(parameters => parameters);
+        Services.AddSingleton<INotesService>(new NotesService());
 
+        var navManager = Services.GetRequiredService<FakeNavigationManager>();
+
+        // Act
         var cut = RenderComponent<NotePage>(parameters => parameters
             .Add(p => p.Id, 1));
 
+        cut.Find("#save-note-btn").Click();
+
         // Assert
-        cut.Find("h1").MarkupMatches("<h1>Shopping list</h1>");
+        Assert.AreEqual(navManager.Uri, "http://localhost/notes");
     }
 }
--- NotesRepositoryTests.cs
+++ NotesRepositoryTests.cs
@@ -1,41 +1,40 @@
-﻿using NoteRocket.Client.Models;
-namespace NoteRocketTests;
+﻿namespace NoteRocketTests;
 
 [TestFixture]
 public class NoteRepositoryTests
 {
 
-	//[Test]
-	//public void UpdateNote_NoteIdDoesntMatchNote_Aborts()
-	//{
-	//    //Setup
+    //[Test]
+    //public void UpdateNote_NoteIdDoesntMatchNote_Aborts()
+    //{
+    //    //Setup
 
-	//    var id = 1;
-	//    var note = new Note { Id = 1, Title = "Shopping list", Body = "carrot, broccoli...", Category = Category.Personal };
+    //    var id = 1;
+    //    var note = new Note { Id = 1, Title = "Shopping list", Body = "carrot, broccoli...", Category = Category.Personal };
 
-	//    //Arrange
+    //    //Arrange
 
-	//    var bob = new NotesService();
-	//    var outcome = bob.UpdateNote(id, note);
+    //    var bob = new NotesService();
+    //    var outcome = bob.UpdateNote(id, note);
 
-	//    //Act
-	//    Assert.AreEqual(outcome, "updated");
-	//}
+    //    //Act
+    //    Assert.AreEqual(outcome, "updated");
+    //}
 
-	[Test]
-	public void UpdateNote_NoteIdDoesntMatchNote_Aborts()
-	{
-		//Setup
+    //[Test]
+    //public void UpdateNote_NoteIdDoesntMatchNote_Aborts()
+    //{
+    //	//Setup
 
-		var id = 1;
-		var note = new Note { Id = 1, Title = "Shopping list", Body = "carrot, broccoli...", Category = Category.Personal };
+    //	var id = 1;
+    //	var note = new Note { Id = 1, Title = "Shopping list", Body = "carrot, broccoli...", Category = Category.Personal };
 
-		//Arrange
+    //	//Arrange
 
-		var bob = new NotesService();
-		var outcome = bob.UpdateNote(id, note);
+    //	var bob = new NotesService();
+    //	var outcome = bob.UpdateNote(id, note);
 
-		//Act
-		Assert.AreEqual(outcome, "updated");
-	}
+    //	//Act
+    //	Assert.AreEqual(outcome, "updated");
+    //}
 }
